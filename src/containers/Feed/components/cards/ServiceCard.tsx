3a5b164c4f68bb6f7340843bc1d253901fe01e67
zzunkie <jeongki@orcar.kr>
2023-02-13 13:57:16 +0900

    feat: ServiceCard 여러개 노출 가능하게 변경

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -8,15 +8,18 @@ const ServiceCard: React.FC = () => {
     <>
       <div className="p-1 mb-3 dark:text-white">🌟 Service</div>
       <ul className="rounded-2xl mb-9 bg-white dark:bg-zinc-700 p-1 flex flex-col">
-        <a
-          href={`${CONFIG.projects[0].href}`}
-          rel="noreferrer"
-          target="_blank"
-          className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
-        >
-          <AiFillCodeSandboxCircle className="text-2xl" />
-          <div className="text-sm">{CONFIG.projects[0].name}</div>
-        </a>
+        {CONFIG.projects.map((project, index) => (
+          <a
+            key={index}
+            href={`${project.href}`}
+            rel="noreferrer"
+            target="_blank"
+            className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+          >
+            <AiFillCodeSandboxCircle className="text-2xl" />
+            <div className="text-sm">{project.name}</div>
+          </a>
+        ))}
       </ul>
     </>
   )
